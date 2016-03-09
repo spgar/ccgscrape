ccdd2dca43edf179fee21dc8a870b9dcbf832bef
Steve Gargolinski <steve.gargolinski@gmail.com>
2016-03-08 19:54:42 -0500

    Basic functionality.

--- server.js
+++ server.js
@@ -4,7 +4,41 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
 
-app.get('/scrape', function(req, res){
+app.get('/scgscrape', function(req, res) {
+    url = 'http://sales.starcitygames.com//deckdatabase/displaydeck.php?DeckID=99447';
+
+    request(url, function(error, response, html) {
+        if (error) {
+            console.log(error);
+        } else {
+            var $ = cheerio.load(html);
+
+            var deckName, playerName, place;
+            var json = { deckName: '', playerName: '', place: 0 };
+
+            $('.deck_title').filter(function() {
+                deckName = $(this).text();
+            });
+
+            $('.player_name').filter(function() {
+                playerName = $(this).text();
+            });
+
+            $('.deck_played_placed').filter(function() {
+                var data = $(this);
+                var placeText = data.text().match(/\d(st|nd|rd|th)\sPlace/);
+                if (placeText) {
+                    place = parseInt(placeText[0][0]);
+                }
+            })
+
+            json.deckName = deckName;
+            json.playerName = playerName;
+            json.place = place;
+
+            console.log(json);
+        }
+    });
 });
 
 app.listen('8081');
