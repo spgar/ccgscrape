5d6ff26095f7b568c7c439e9ae51df5d1b944af8
Steve Gargolinski <steve.gargolinski@gmail.com>
2016-04-03 09:47:56 -0400

    Adds additional tests.

--- test/scraper.js
+++ test/scraper.js
@@ -3,11 +3,31 @@ var scraper = require('../scraper');
 
 describe('Deck Scraper', function() {
     describe('Add to deck', function() {
-        it('adds a single card to the deck', function() {
+        it('adds a single card to the maindeck', function() {
             var cards = [];
             scraper.addCard(cards, '1 Underground Sea', false);
             expect(cards.length).to.equal(1);
             expect(cards).to.deep.equal([{name: 'Underground Sea', main: 1, side: 0}]);
+        });
+        it('adds a single card to the sideboard', function() {
+            var cards = [];
+            scraper.addCard(cards, '1 Underground Sea', true);
+            expect(cards.length).to.equal(1);
+            expect(cards).to.deep.equal([{name: 'Underground Sea', main: 0, side: 1}]);
+        });
+        it('does not create duplicate cards in the deck', function() {
+            var cards = [];
+            scraper.addCard(cards, '1 Underground Sea', false);
+            expect(function() {
+                scraper.addCard(cards, '1 Underground Sea', false);
+            }).to.throw('Trying to add the same card twice: Underground Sea');
+        });
+        it('adds cards to maindeck and sideboard', function() {
+            var cards = [];
+            scraper.addCard(cards, '1 Underground Sea', false);
+            scraper.addCard(cards, '1 Underground Sea', true);
+            expect(cards.length).to.equal(1);
+            expect(cards).to.deep.equal([{name: 'Underground Sea', main: 1, side: 1}]);
         });
     });
 });
