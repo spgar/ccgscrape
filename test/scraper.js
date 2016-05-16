751060e93cc2d8bee676e853285f97fdf219a463
Steve Gargolinski <steve.gargolinski@gmail.com>
2016-05-16 11:52:04 -0400

    Moves test data into .json file

--- test/scraper.js
+++ test/scraper.js
@@ -1,5 +1,6 @@
 var expect  = require('chai').expect;
 var bluebird = require('bluebird');
+var jsonfile = require('jsonfile');
 var scraper = bluebird.promisifyAll(require('../scraper'));
 
 describe('Deck Scraper', function() {
@@ -44,31 +45,8 @@ describe('Deck Scraper', function() {
         it('scrapes a complete deck', function(done) {
             scraper.scrapeHearthpwnDeckAsync(249225)
             .then(function(deckJSON) {
-                expect(deckJSON.deckName).to.equal('[S14] Combo Druid');
-                expect(deckJSON.playerName).to.equal('Alphros');
-                expect(deckJSON.deckClass).to.equal('Druid');
-                expect(deckJSON.craftingCost).to.equal(8940);
-                expect(deckJSON.cards).to.deep.equal([
-                    { name: 'Innervate', quantity: 2 },
-                    { name: 'Wild Growth', quantity: 2 },
-                    { name: 'Wrath', quantity: 2 },
-                    { name: 'Savage Roar', quantity: 2 },
-                    { name: 'Keeper of the Grove', quantity: 2 },
-                    { name: 'Swipe', quantity: 2 },
-                    { name: 'Druid of the Claw', quantity: 2 },
-                    { name: 'Force of Nature', quantity: 2 },
-                    { name: 'Ancient of Lore', quantity: 2 },
-                    { name: 'Shade of Naxxramas', quantity: 2 },
-                    { name: 'Piloted Shredder', quantity: 2 },
-                    { name: 'Azure Drake', quantity: 1 },
-                    { name: 'Big Game Hunter', quantity: 1 },
-                    { name: 'Harrison Jones', quantity: 1 },
-                    { name: 'Sludge Belcher', quantity: 1 },
-                    { name: 'Emperor Thaurissan', quantity: 1 },
-                    { name: 'Sylvanas Windrunner', quantity: 1 },
-                    { name: 'Dr. Boom', quantity: 1 },
-                    { name: 'Ragnaros the Firelord', quantity: 1 }
-                ]);
+                var testJSON = jsonfile.readFileSync('./test/data/hearthpwn_249225.json');
+                expect(deckJSON).to.deep.equal(testJSON);
                 done();
             });
         });
